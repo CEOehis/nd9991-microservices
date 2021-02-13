f5eeb15f4f093b29ff8cd9ae9be7aadca897e4e7
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2021-02-13 12:53:37 +0100

    Add script for uploading docker build

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,13 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ceoehis/ml-microservice
 
-# Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username ceoehis
+docker tag ml-microservice:latest ${dockerpath}:latest
 
-# Step 3:
 # Push image to a docker repository
+docker push $dockerpath

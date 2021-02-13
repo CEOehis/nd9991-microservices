56f93695e03281b1feed1f5c315940bb072778cd
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2021-02-13 16:44:01 +0100

    Add script for running docker container in kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,16 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ceoehis/ml-microservice
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run ml-prediction-service --image=$dockerpath --port=80
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
-
+# Wait for 10 seconds for pod to be in READY state
+sleep 10
+kubectl port-forward ml-prediction-service 8000:80
